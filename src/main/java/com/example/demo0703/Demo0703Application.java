6632046ff448bd7f78d989d1ec967aaa34fd06b2
wakupipe <3013322982@qq.com>
2025-07-04 09:43:39 +0800

    Initial commit - 上传已有代码

---
+++ src/main/java/com/example/demo0703/Demo0703Application.java
@@ -0,0 +1,13 @@
+package com.example.demo0703;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0703Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo0703Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo0703/Demo0703ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo0703;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0703ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
